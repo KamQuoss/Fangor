70b8d20e51ea3733c4678ae24c743b4a2418b3c8
KamQuoss <kamilaklosek@gmail.com>
2021-02-05 22:34:23 +0100

    scratch varsion

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let figure = document.querySelector(".image-content"),
+backgrounds = [
+ "rgba(99, 177, 252, 1) 0%, rgba(77, 136, 202, 1) 10%, rgba(9, 9, 121, 1) 16%, rgba(6, 0, 55, 1) 31%, rgba(9, 9, 121, 1) 46%,  rgba(99, 8, 235, 1) 47%, rgba(255, 255, 255, 1) 50%",
+ "rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 11%, rgba(16, 108, 213, 1) 20%, rgba(21, 44, 75, 1) 24%, rgba(48, 82, 38, 1) 35%, rgba(48, 82, 38, 1) 54%, rgba(114, 150, 6, 1) 58%, rgba(227, 250, 79, 1) 62%, rgba(255, 251, 235, 1) 68%, rgba(255, 251, 235, 1) 100%",
+ "rgba(255,221,0,1) 11%, rgba(154,38,62,1) 30%, rgba(154,38,62,1) 47%, rgba(255,221,0,1) 63%",
+ "rgba(254,254,254,1) 12%, rgba(161,122,196,1) 21%, rgba(48,103,184,1) 28%, rgba(58,161,105,1) 33%, rgba(58,161,105,1) 58%, rgba(48,203,177,1) 64%, rgba(189,187,252,1) 70%",
+ "rgba(242,238,229,1) 10%, rgba(6,127,184,1) 26%, rgba(0,33,112,1) 35%, rgba(0,33,112,1) 37%, rgba(6,109,184,1) 46%, rgba(242,238,229,1) 58%",
+ "rgba(31,50,105,1) 16%, rgba(42,128,191,1) 19%, rgba(242,238,229,1) 25%, rgba(242,229,229,1) 33%, rgba(242,238,229,1) 44%, rgba(42,128,191,1) 52%, rgba(25,92,166,1) 54%, rgba(25,92,166,1) 56%, rgba(189,177,181,1) 64%",
+ "rgba(196,86,86,1) 28%, rgba(122,171,59,1) 39%, rgba(122,171,59,1) 46%, rgba(215,89,136,1) 60%",
+ "rgba(157,211,226,1) 21%, rgba(102,196,193,1) 27%, rgba(255,157,9,1) 34%, rgba(255,157,9,1) 47%, rgba(210,142,41,1) 53%, rgba(196,168,227,1) 60%"
+];
+
+const drawBackground = () => {
+    let colorArray = backgrounds,
+    randomElem = Math.floor(Math.random() * colorArray.length);
+    return randomElem
+}
+
+const changeFigure = () => {
+    figure.style.background = `radial-gradient( circle, ${backgrounds[drawBackground()]})`;
+}
+window.document.addEventListener("click", changeFigure)
